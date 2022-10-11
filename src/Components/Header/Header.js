fcc0e1f410425c5c967dbec8b0c9679af50de9b8
Mamun2k22 <mamun180037@gmail.com>
2022-10-11 23:38:16 +0600

    first comit, Create Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <Link to={'/home'}>Home</Link>
+            <Link to={'/statistics'}>Statistics</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/blog'}>Blog</Link>
+        </nav>
+    );
+};
+
+export default Header;
