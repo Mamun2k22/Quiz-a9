cf9bf5ffbd9c24a742c98562bd8e15c51e6a6c1d
Mamun2k22 <mamun180037@gmail.com>
2022-10-12 11:41:17 +0600

    4th repo

--- src/Components/Quiz/Singlequiz/Singlequiz.js
+++ src/Components/Quiz/Singlequiz/Singlequiz.js
@@ -6,13 +6,13 @@ const Singlequiz = ({ qz }) => {
     return (
 
         <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
-            <img src={logo} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+            <img src={logo} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500 bg-yellow-300" />
             <div className="flex flex-col justify-between p-6 space-y-8">
                 <div className="space-y-2">
                     <h2 className="text-3xl font-semibold tracking-wide">{name}</h2>
                     <p className="dark:text-gray-100">{total}</p>
                 </div>
-                <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900">Read more</button>
+                <button type="button" className="flex items-center justify-center w-full p-3 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900 bg-red-600">Quiz</button>
             </div>
         </div>
 
